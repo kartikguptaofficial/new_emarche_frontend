877b0c628f2e0c6e090f116c500253e344ed906c
Kartik <kartikgupta962004@gmail.com>
2022-10-25 18:42:02 +0530

    added new features

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -235,7 +235,7 @@ export default function Navbar() {
                 </li>
                 </>:
                 <li>
-                    <a href='' type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">LOGIN</a>
+                    <a href='/login' type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">LOGIN</a>
                 </li> 
             }
 
